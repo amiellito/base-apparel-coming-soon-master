adcc61acb7ed64147d6f0ced71fa9a4a2fb269cd
amiellito <amiel.lapuebla@gmail.com>
2020-04-24 15:51:55 -0700

    Added validation in js

---
+++ script.js
@@ -0,0 +1,34 @@
+const email = document.getElementById("email");
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "formControl error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "formControl success";
+}
+
+//Check if email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+//Event listeners
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+});
